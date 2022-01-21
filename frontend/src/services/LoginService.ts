bfa395d21f7360b391eee5833dc21c60b56a7c22
Jannis Aziz Khammas <aziz_khammas@yahoo.de>
2022-01-21 15:47:09 +0100

    Refactored frontend userLogin & services

--- frontend/src/services/LoginService.ts
+++ frontend/src/services/LoginService.ts
@@ -1,27 +1,41 @@
 import {AxiosResponse} from "axios";
 
-const axios = require('axios').default;
+// Todo: check for and delete old/invalid/expired token (request new login)
+const AUTH_TOKEN_STORAGE_KEY = "AUTH_TOKEN"
 
-const userData = {username: "", authToken: ""}
 
-export const getUserData = () => ({...userData}) // Copy for safety
+// USERDATA / TOKEN ACCESS
+const getToken = () : string => localStorage.getItem(AUTH_TOKEN_STORAGE_KEY) || ""
+const setToken = (token: string) : void => {localStorage.setItem(AUTH_TOKEN_STORAGE_KEY, token)}
+const removeToken = () : void => localStorage.removeItem(AUTH_TOKEN_STORAGE_KEY)
 
-export const isLoggedIn = () => userData.authToken !== ""
+export const getUsername = () : string => extractUsernameFromToken(getToken())
+export const logOut = () : void => removeToken()
+export const isLoggedIn = ():boolean => !!getToken() // !! = !("" || null || undefined)
 
-export const logOut = () => userData.username = userData.authToken = ""
+const getAxiosConfig = () : {headers: {'Authorization': string}} => ({headers: {'Authorization': 'Bearer ' + getToken()}})
 
-const getConfig = () => ({headers: {'Authorization': 'Bearer ' + getUserData().authToken}})
 
 // USER LOGIN & REGISTER
-export const hello = () =>
-    axios.get("/auth/", getConfig()).then( (response:AxiosResponse) => response.data)
+const axios = require('axios').default;
 
-export const login = (userInput: {username: string, password: string}) =>
-    axios.post("/auth/login/", userInput)
-        .then( (response:AxiosResponse<string>) => {
-            userData.username = userInput.username
-            userData.authToken = response.data
-        })
+export const hello = () : Promise<string> =>
+    axios.get("/user/", getAxiosConfig()).then( (response:AxiosResponse) => response.data)
 
-export const register = (userInput: {username: String, password: String}) =>
+export const login = (userInput: {username: string, password: string}) : Promise<void> =>
+    axios.post("/auth/login/", userInput).then(
+        (response:AxiosResponse<string>) => setToken(response.data))
+
+export const register = (userInput: {username: String, password: String}) : Promise<string> =>
     axios.post("/auth/register/", userInput).then( (response:AxiosResponse) => response.data)
+
+
+// Vanilla token data extraction
+const extractUsernameFromToken = (token: string):string => {
+    if (!token) { return ""}
+    // token: HEADER.>DATA<.SIGNATURE
+    const base64Url = token.split('.')[1];
+    // DATA: {"sub": "1234", >"name": "username"<, "iat": "17125"}
+    const base64 = base64Url.replace('-', '+').replace('_', '/');
+    return JSON.parse(window.atob(base64)).sub;
+}
